dd475e652a427b148151551397e99fb793260627
M Arka Kharisma <mkharisma11@gmail.com>
2017-05-12 23:57:49 +0700

    First Commit For EduFun Application

---
+++ src/EduFun.java
@@ -0,0 +1,19 @@
+/*
+    Aplikasi EduFun Untuk Media Belajar Bagi Anak-Anak
+
+    Developer : - Muhammad Arka Kharisma (151402081)
+                - Virliansi Adrisa Utami (151402076)
+ */
+
+import javax.swing.*;
+
+public class EduFun extends JFrame {
+    public EduFun()
+    {
+        System.out.println("Test");
+    }
+    public static void main(String[] args)
+    {
+        EduFun app = new EduFun();
+    }
+}
