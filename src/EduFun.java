141ff62d62338aaf68c3a8f3fff59bec2d46573f
Virli151402076 <thunderonseptember@gmail.com>
2017-05-23 22:20:02 +0700

    Update EduFun.java
    Penambahan button play

--- src/EduFun.java
+++ src/EduFun.java
@@ -6,14 +6,34 @@
  */
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class EduFun extends JFrame {
+    private JButton play;
     public EduFun()
     {
-        System.out.println("Test");
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(0, 1));
+
+        p1.add(play = new JButton("PLAY"));
+        play.addActionListener(new listenerPlay());
+
+        add(p1);
+    }
+
+    class listenerPlay implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Game Start");
+        }
     }
     public static void main(String[] args)
     {
         EduFun app = new EduFun();
+        app.pack(); //Agar ukuran frame menyesuaikan
+        app.setLocationRelativeTo(null);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setVisible(true);
     }
 }
