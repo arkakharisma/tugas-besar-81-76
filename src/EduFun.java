084e281fbbb27b5719eff68fecff3a9f23c78375
Virli151402076 <thunderonseptember@gmail.com>
2017-05-30 20:42:39 +0700

    Update EduFun.java
    Penambahan background dan auto maximize

--- src/EduFun.java
+++ src/EduFun.java
@@ -11,23 +11,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class EduFun extends JFrame {
-    private JButton play;
+    //private JButton play;
+
     public EduFun()
     {
+        setTitle("Edu-Fun: Great Education for Kids");
+        JButton play = new JButton("PLAY");
+
         JPanel p1 = new JPanel();
-        p1.setLayout(new GridLayout(0, 1));
+            p1.setLayout(new GridLayout(0, 1));
+            p1.add(play);
+                play.addActionListener(new listenerPlay());
 
-        p1.add(play = new JButton("PLAY"));
-        play.addActionListener(new listenerPlay());
 
+        setLayout(new BorderLayout());
+        setContentPane(new JLabel(new ImageIcon("src/Lighthouse.jpg")));
+
+        setLayout(new FlowLayout());
         add(p1);
     }
+
 
     class listenerPlay implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             System.out.println("Game Start");
         }
     }
+
     public static void main(String[] args)
     {
         EduFun app = new EduFun();
@@ -35,5 +45,7 @@ public class EduFun extends JFrame {
         app.setLocationRelativeTo(null);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
+        app.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
     }
 }
